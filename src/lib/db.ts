56599b42681423cc6c56caa1a80c417a258c6d3b
shagara2112 <86645965+shagara2112@users.noreply.github.com>
2025-10-10 18:49:12 +0800

    fix: update database connection to resolve prepared statement errors

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -9,7 +9,7 @@ export const db = globalForPrisma.prisma ?? new PrismaClient({
   log: ['query'],
   datasources: {
     db: {
-      url: process.env.DATABASE_URL
+      url: process.env.DATABASE_URL + "?connection_limit=10&pool_timeout=20"
     }
   }
 })
